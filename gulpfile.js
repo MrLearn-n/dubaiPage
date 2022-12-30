ed8fe636a4dd0473ddc7a4eb82df6bed41ea16a2
MrLearn <learnmr67@gmail.com>
2022-12-31 00:01:34 +0300

    наладил task'и, сделал их watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,34 @@ const pug = require('gulp-pug');
 
 
 gulp.task('taskPug', function() {
-    return gulp.src('src/pug/index.pug')
+    return gulp.src('./src/pug/**/**')
         .pipe(pug())
-        .pipe(gulp.src('src/pug/index.html'));
+        .pipe(gulp.dest('./dist/html'));
 })
 
 gulp.task('taskScss', function() {
-    return gulp.src('src/sass/media.scss')
+    return gulp.src('./src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('style/*.css'));
+        .pipe(gulp.dest('./dist/css'));
 })
     
+
+gulp.task('taskFonts', function() {
+    return gulp.src('./src/fonts/**/**')
+        .pipe(gulp.dest('./dist/fonts'));
+})
+
+gulp.task('taskImgs', function() {
+    return gulp.src('./src/imgs/**/**')
+        .pipe(gulp.dest('./dist/imgs'));
+})
+
+gulp.task('watch', function() {
+    gulp.watch('./src/pug/*.pug', gulp.series(['taskPug']));
+    gulp.watch('./src/sass/*.scss', gulp.series(['taskScss']));
+    gulp.watch('./src/fonts/**/**', gulp.series(['taskFonts']));
+    gulp.watch('./src/imgs/**/**', gulp.series(['taskImgs']));
+})
+
+gulp.task('default', gulp.series(['taskPug', 'taskScss', 'taskFonts', 'taskImgs', 'watch']));
+
